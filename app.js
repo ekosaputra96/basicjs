bda74a174dcd52f8f9cd01e5b0ce946bd88bb629
eko saputra <ekosaputra1331@gmail.com>
2022-06-11 15:54:42 +0700

    password

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log("HELLO EVERYBODY");
-let num = prompt("your age ?  ");
-console.log(num);
+let password = prompt("Enter your password : ");
+
+if(password.length >= 6){
+    if(password.indexOf(' ') === -1){
+        console.log("Valid password");
+    }else {
+        console.log("No spaces allowed");
+    }
+}else {
+    console.log("Password too short !");
+}
