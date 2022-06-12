835398df7affce56d3dbf5fb676b60af4e2b7d01
eko saputra <ekosaputra1331@gmail.com>
2022-06-12 15:16:31 +0700

    menu

--- app.js
+++ app.js
@@ -17,13 +17,29 @@
 // 11 - 65 $20
 // 65+ free
 
-let age = 80;
-if(age >= 0 && age <=5 || age >= 65){
-    console.log("Free");
-}else if(age >= 6 && age <= 10){
-    console.log("$10");
-}else if(age >= 11 && age <= 65){
-    console.log("$20");
-}else{
-    console.log("INVALID AGE !");
+// let age = 80;
+// if((age >= 0 && age <=5) || age >= 65){
+//     console.log("Free");
+// }else if(age >= 6 && age <= 10){
+//     console.log("$10");
+// }else if(age >= 11 && age <= 65){
+//     console.log("$20");
+// }else{
+//     console.log("INVALID AGE !");
+// }
+
+console.log("MENU");
+const menu = parseInt(prompt("Enter Menu : 1. Lihat 2. Pesan 3. Bayar"));
+switch(menu){
+    case 1:
+        console.log("Lihat Menu");
+        break;
+    case 2:
+        console.log("Pesan Menu");
+        break;
+    case 3:
+        console.log("Bayar Pesanan");
+        break;
+    default:
+        console.log("INVALID!");
 }
